73c12846968d04403d101c26d157e788b4664b34
BGC1990 <bencrayston1990@gmail.com>
2022-10-20 23:38:50 +0100

    DateTimeInterface Problems

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -2,6 +2,7 @@
 // src/Controller/TaskController.php
 namespace App\Controller;
 
+use App\Entity\Employee;
 use App\Entity\FormEmployee;
 use App\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,17 +12,32 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Form\Type\TaskType;
+use Doctrine\Persistence\ManagerRegistry;
 
 class TaskController extends AbstractController
 {
-    public function new(): Response
+    public function new(Request $request, ManagerRegistry $doctrine): Response
     {
+        $entityManager = $doctrine->getManager();
         // creates a task object and initializes some data for this example
-        $FormEmployee = new FormEmployee();
-        $FormEmployee->setFirst_Name('First Name');
-        $FormEmployee->setLast_Name('Last Name');
+       // $FormEmployee = new FormEmployee();
+       // $FormEmployee->setFirst_Name('');
+       // $FormEmployee->setLast_Name('');
 
-        $form = $this->createForm(TaskType::class, $FormEmployee);
+        $form = $this->createForm(TaskType::class);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+            
+            $employee = new Employee();
+            $employee->setFirstName($data['first_name']);
+            $employee->setLastName($data['last_name']);
+            $employee->setDob($data['date_of_birth']);
+            $employee->setEmail($data['email_address']);
+            $entityManager->persist($employee);
+            $entityManager->flush();
+        }
 
         return $this->renderForm('task/new.html.twig', [
             'form' => $form,

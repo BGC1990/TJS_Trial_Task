cbca539a9dd84fbcf6c7749865b8e2ecc32477c4
BGC1990 <bencrayston1990@gmail.com>
2022-10-21 14:34:08 +0100

    Validation Hiccups
    Attempting to use the Validator interface

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -13,10 +13,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use App\Form\Type\TaskType;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class TaskController extends AbstractController
 {
-    public function new(Request $request, ManagerRegistry $doctrine): Response
+    public function new(Request $request, ManagerRegistry $doctrine, ValidatorInterface $validator): Response
     {
         $entityManager = $doctrine->getManager();
         // creates a task object and initializes some data for this example
@@ -29,8 +30,10 @@ class TaskController extends AbstractController
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
             $data = $form->getData();
-            
             $employee = new Employee();
+            $errors = $validator->validate($employee);
+            if (!$errors)
+            {
             $employee->setFirstName($data['first_name']);
             $employee->setLastName($data['last_name']);
             $dob = $data['date_of_birth'];
@@ -39,6 +42,7 @@ class TaskController extends AbstractController
             $employee->setEmail($data['email_address']);
             $entityManager->persist($employee);
             $entityManager->flush();
+            }
         }
 
         return $this->renderForm('task/new.html.twig', [
